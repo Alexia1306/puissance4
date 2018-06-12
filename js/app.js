2e8a9f513e30730ea4ee1916f4c7a15d19a375bb
hermelen <Hermelen>
2018-06-12 14:58:56 +0200

    else if a remplacer par for

--- js/app.js
+++ js/app.js
@@ -3,31 +3,23 @@ var titre = document.getElementsByTagName('h1')[0];
 var bodyA = document.getElementsByTagName('body')[0];
 var tableA = document.createElement("table");
 
-var matrixTable = [];
+
 var trA = [];
 
 for (var j = 0; j < 6; j++) {
-    matrixTable[j] = [];
     trA[j] = document.createElement("tr");
     trAj = trA[j];
-    var tdA = [];
     for (var i = 0; i < 7; i++) {
-        matrixTable[j].push('0');
-        tdA[i] = document.createElement("td");
-        tdAi = tdA[i];
+        trA[j][i] = document.createElement("td");
+        tdAi = trA[j][i];
         tdAi.classList.add('tableA');
-        tdAi.innerHTML = "0";
+        tdAi.style.backgroundColor = "grey";
         trAj.appendChild(tdAi);
     }
     tableA.appendChild(trAj);
 }
 
 bodyA.prepend(tableA);
-
-
-
-matrixTable[2][3] = "1";
-matrixTable[0][3] = "5";
 
 var tableB = document.createElement("table");
 var trB = document.createElement("tr");
@@ -38,19 +30,24 @@ for (var i = 0; i < 7; i++) {
     tdBi.classList.add('tableB');
     tdBi.id = i;
     tdBi.addEventListener('click',function() {
-        matrixTable[5][i] = "1";
-        console.log(matrixTable);
-    }
-    )
+      if (trA[5][this.id].style.backgroundColor == "grey") {
+        trA[5][this.id].style.backgroundColor = "blue";
+      } else if (trA[4][this.id].style.backgroundColor == "grey"){
+        trA[4][this.id].style.backgroundColor = "blue";
+      } else if (trA[3][this.id].style.backgroundColor == "grey"){
+        trA[3][this.id].style.backgroundColor = "blue";
+      } else if (trA[2][this.id].style.backgroundColor == "grey"){
+        trA[2][this.id].style.backgroundColor = "blue";
+      } else if (trA[1][this.id].style.backgroundColor == "grey"){
+        trA[1][this.id].style.backgroundColor = "blue";
+      } else {
+        trA[0][this.id].style.backgroundColor = "blue";
+      }
+    });
 
     trB.appendChild(tdBi);
 }
 tableB.appendChild(trB);
 bodyA.prepend(tableB);
 
-unTd = document.getElementsByClassName('tableA');
-console.log(unTd);
-
-// function addJetons(id) {
-//     matrixTable[5] = "1";
-// }
+console.log(trA[5][1]);
