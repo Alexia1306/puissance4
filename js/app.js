71217493e1b1b161daec432c02a9956febc247c8
zampiello alexia <alexzamp@hotmail.fr>
2018-06-12 10:10:56 +0200

    mise en place du debut html css et js

---
+++ js/app.js
@@ -0,0 +1,9 @@
+var bodyA = document.getElementsByTagName('body')[0];
+var tableA = document.createElement("table");
+
+var trA = document.createElement("tr");
+var tdA = [];
+    for (var i = 0; i < 7; i++) {
+         tdA[i] = document.createElement("td");
+    }
+console.log(bodyA);
