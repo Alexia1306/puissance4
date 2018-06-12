92c6cdd5e18b3e421c0545213b247052104ade41
hermelen <Hermelen>
2018-06-12 13:36:04 +0200

    fusion Matrix and HTML script

--- js/app.js
+++ js/app.js
@@ -7,15 +7,20 @@ var bodyA = document.getElementsByTagName('body')[0];
 
 var tableA = document.createElement("table");
 
+var matrixTable = [];
 var trA = [];
 
 for (var j = 0; j < 6; j++) {
+    matrixTable[j] = [];
     trA[j] = document.createElement("tr");
     trAj = trA[j];
     var tdA = [];
     for (var i = 0; i < 7; i++) {
+        matrixTable[j].push('0');
         tdA[i] = document.createElement("td");
         tdAi = tdA[i];
+        tdAi.classList.add('tableA');
+        tdAi.innerHTML = "0";
         trAj.appendChild(tdAi);
     }
     tableA.appendChild(trAj);
@@ -23,16 +28,10 @@ for (var j = 0; j < 6; j++) {
 
 bodyA.prepend(tableA);
 
-// create array data
-var matrixTable = [];
 
-for (var j = 0; j < 6; j++) {
-    matrixTable[j] = [];
-    for (var i = 0; i < 7; i++) {
-        matrixTable[j].push('0');
-    }
-}
+
 matrixTable[2][3] = "1";
+matrixTable[0][3] = "5";
 
 var tableB = document.createElement("table");
 var trB = document.createElement("tr");
@@ -40,8 +39,16 @@ var tdB = [];
 for (var i = 0; i < 7; i++) {
     tdB[i] = document.createElement("td");
     tdBi = tdB[i];
+    tdBi.classList.add('tableB');
+    tdBi.id = i;
+    tdBi.addEventListener('click', function(){
+      console.log(this.id);
+    })
     trB.appendChild(tdBi);
 }
 tableB.appendChild(trB);
 bodyA.prepend(tableB);
 console.log(matrixTable);
+
+unTd = document.getElementsByClassName('tableA');
+console.log(unTd);
