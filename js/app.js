769511d68b7c98ac17b6ab99d877904fde41aa57
hermelen <Hermelen>
2018-06-15 13:49:45 +0200

    slide jetons

--- js/app.js
+++ js/app.js
@@ -4,8 +4,8 @@ var mainElmt = document.getElementsByTagName('main')[0];
 var tableA = document.createElement("table");
 var joueur = 1;
 
-
 var trA = [];
+// var tableData = [];
 
 for (var j = 0; j < 6; j++) {
   trA[j] = document.createElement("tr");
@@ -13,8 +13,7 @@ for (var j = 0; j < 6; j++) {
   for (var i = 0; i < 7; i++) {
     trA[j][i] = document.createElement("td");
     tdAi = trA[j][i];
-    // tdAi.classList.add('tableA');
-    tdAi.style.backgroundColor = "grey";
+    tdAi.className = "grey";
     trAj.appendChild(tdAi);
   }
   tableA.appendChild(trAj);
@@ -35,42 +34,27 @@ for (var i = 0; i < 7; i++) {
       mainElmt.className = "player_2";
       color = "blue";
       joueur = 2;
+      jeton = document.createElement('div');
+      jeton.style.backgroundColor = color;
+      jeton.className = "jeton";
     } else if (joueur == 2) {
       mainElmt.className = "player_1";
       color = "red";
       joueur = 1;
+      jeton = document.createElement('div');
+      jeton.style.backgroundColor = color;
+      jeton.className = "jeton";
     }
     for (var i = 5; i >= 0; i--) {
-      if (trA[i][this.id].style.backgroundColor == "grey") {
-        trA[i][this.id].style.backgroundColor = color;
-        for (var j = 0; j < 4; j++) {
-          if (trA[i][j].style.backgroundColor == color && trA[i][j+1].style.backgroundColor == color && trA[i][j+2].style.backgroundColor == color && trA[i][j+3].style.backgroundColor == color) {
-            if (!alert('victoire de ' + color + '!')) {
-              window.location.reload()
-            }
-          }
-        }
-        if (i < 3) {
-          if (trA[i + 1][this.id].style.backgroundColor == color && trA[i + 2][this.id].style.backgroundColor == color && trA[i + 3][this.id].style.backgroundColor == color) {
-            if (!alert('victoire de ' + color + '!')) {
-              window.location.reload()
-            }
-          }
-          if (this.id > 2) {
-            if (trA[i + 1][this.id - 1].style.backgroundColor == color && trA[i + 2][this.id - 2].style.backgroundColor == color && trA[i + 3][this.id - 3].style.backgroundColor == color) {
-              if (!alert('victoire de ' + color + '!')) {
-                window.location.reload()
-              }
-            }
-          }
-          if (this.id < 4) {
-            if (trA[i - 1][this.id + 1].style.backgroundColor == color && trA[i - 2][this.id + 2].style.backgroundColor == color && trA[i - 3][this.id + 3].style.backgroundColor == color) {
-              if (!alert('victoire de ' + color + '!')) {
-                window.location.reload()
-              }
-            }
-          }
-        }
+      row = i;
+      col = parseInt(this.id);
+      if (trA[row][col].className == "grey") {
+        trA[row][col].className = color;
+        trA[row][col].appendChild(jeton);
+        setTimeout(function(){
+          jeton.classList.add('bottom');
+        }, 100)
+        checkWin();
         break;
       }
     }
@@ -81,3 +65,44 @@ for (var i = 0; i < 7; i++) {
 }
 tableB.appendChild(trB);
 mainElmt.prepend(tableB);
+
+function checkWin() {
+  // console.log(trA);
+
+  // for (var j = 1; j < 8; j++) {
+  //   if (trA[row][col+j]){
+  //     console.log(trA[row][col+j].children[0].style.backgroundColor);
+  //   }
+  // }
+
+  for (var j = 0; j < 4; j++) {
+    if (trA[row][j].className == color && trA[row][j+1].className == color && trA[row][j+2].className == color && trA[row][j+3].className == color) {
+      youWin(color)
+    }
+  }
+  if (i < 3) {
+    if (trA[row + 1][col].className == color && trA[row + 2][col].className == color && trA[row + 3][col].className == color) {
+      youWin(color)
+    }
+    if (col > 2) {
+      if (trA[row + 1][col - 1].className == color && trA[row + 2][col - 2].className == color && trA[row + 3][col - 3].className == color) {
+        youWin(color)
+      }
+    }
+    if (col < 4) {
+      if (trA[row + 1][col + 1].className == color && trA[row + 2][col + 2].className == color && trA[row + 3][col + 3].className == color) {
+        youWin(color)
+      }
+    }
+  }
+}
+
+function youWin(color){
+  var winDiv = document.createElement('div');
+  winDiv.className = "win";
+  winDiv.innerHTML = "Victoire de "+color+"!";
+  mainElmt.appendChild(winDiv);
+  setTimeout(function(){
+    window.location.reload()
+  }, 3000)
+}
